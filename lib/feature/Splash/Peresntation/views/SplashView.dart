3119347de9b419e8efdd0fafd0de627c25a95afb
Lahlabooo <ellahloby22@gmail.com>
2024-08-30 22:45:02 +0300

    create SplashView file

---
+++ lib/feature/Splash/Peresntation/views/SplashView.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/feature/Splash/Peresntation/views/widgets/SplashViewBody.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
