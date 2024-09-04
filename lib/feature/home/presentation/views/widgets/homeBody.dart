68f1dcadbe9db7b847a6e1c2606e1b7cc1527f3d
Lahlabooo <ellahloby22@gmail.com>
2024-09-05 00:05:48 +0300

    add Package skeletonizer to app

--- lib/feature/home/presentation/views/widgets/homeBody.dart
+++ lib/feature/home/presentation/views/widgets/homeBody.dart
@@ -4,6 +4,7 @@ import 'package:bookly/feature/home/presentation/views/widgets/CustomAppBar.dart
 import 'package:bookly/feature/home/presentation/views/widgets/HomeItemslistView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -12,30 +13,34 @@ class HomeBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 25.0),
-      child: CustomScrollView(
-        physics: BouncingScrollPhysics(),
-        slivers: [
-          SliverToBoxAdapter(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              CustomAppBar(),
-              ItemsListView(),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                "Best seller ",
-                style: Styels.textStyle18,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-            ]),
-          ),
-          SliverFillRemaining(
-            child: BestSellerItemList(),
-          ),
-        ],
+      child: Skeletonizer(
+        enabled: false,
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomAppBar(),
+                    ItemsListView(),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Best seller ",
+                      style: Styels.textStyle18,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ]),
+            ),
+            SliverFillRemaining(
+              child: BestSellerItemList(),
+            ),
+          ],
+        ),
       ),
     );
   }
