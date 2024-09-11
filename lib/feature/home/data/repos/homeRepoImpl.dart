01c95eb3facc369982b0dff0076e023759fae3d1
Lahlabooo <ellahloby22@gmail.com>
2024-09-11 18:46:26 +0300

    implement fetchFeaturedBooks methods

--- lib/feature/home/data/repos/homeRepoImpl.dart
+++ lib/feature/home/data/repos/homeRepoImpl.dart
@@ -1,13 +1,27 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utilis/Api_Services.dart';
 import 'package:bookly/feature/home/data/Models/book_model/book_model.dart';
 import 'package:bookly/feature/home/data/repos/homeRepo.dart';
 import 'package:dartz/dartz.dart';
 
-class homeRepoImpl implements HomeRepo {
+class HomeRepoImpl implements HomeRepo {
+  final ApiServer apiServer;
+
+  HomeRepoImpl(this.apiServer);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiServer.Get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+      List<BookModel> books = [];
+
+      for (var element in data['items']) {
+        books.add(BookModel.fromJson(element));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServicesError());
+    }
   }
 
   @override
