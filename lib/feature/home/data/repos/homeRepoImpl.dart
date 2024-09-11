e28fb66998dbe81dab9755ebdf487c2129923c75
Lahlabooo <ellahloby22@gmail.com>
2024-09-11 20:11:36 +0300

    handel errors class

--- lib/feature/home/data/repos/homeRepoImpl.dart
+++ lib/feature/home/data/repos/homeRepoImpl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utilis/Api_Services.dart';
 import 'package:bookly/feature/home/data/Models/book_model/book_model.dart';
 import 'package:bookly/feature/home/data/repos/homeRepo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiServer apiServer;
@@ -21,13 +22,29 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } on Exception catch (e) {
-      return left(ServicesError());
+      if (e is DioException) {
+        return left(ServicesError.fromDioExeption(e));
+      }
+      return left(ServicesError(e.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchNewsetBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiServer.Get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+      List<BookModel> books = [];
+
+      for (var element in data['items']) {
+        books.add(BookModel.fromJson(element));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServicesError.fromDioExeption(e));
+      }
+      return left(ServicesError(e.toString()));
+    }
   }
 }
